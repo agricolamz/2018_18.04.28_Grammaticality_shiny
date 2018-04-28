1e4acaf8b35dcdcd7d0ba7cc4a4356e8e1aaf90b
agricolamz <agricolamz@gmail.com>
2018-04-28 09:06:36 +0300

    add heroku staff

---
+++ init.R
@@ -0,0 +1,9 @@
+my_packages = c("shiny", "ggplot2", "dplyr", "Compositional")
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
